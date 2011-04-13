eebc8870852af6d95697768843d7466a5f9c6369
Jeroen van Ingen <jeroeningen@gmail.com>
2011-04-13 10:40:57 -0700

    README updated

--- vendor/plugins/geox/install.rb
+++
@@ -1,2 +0,0 @@
-# Install hook code here
-puts "Don't forget to view the README file for basic usage/install instructions!"
--- vendor/plugins/geox/lib/geox_api_keys.rb
+++
@@ -1,8 +0,0 @@
-# Put any api keys you need in this file if you must
-# but it's better to copy this file to your config folder in your rails application
-# the system will automatically load that file in preference to this one
-module GeoX
-  MAPQUEST_API_KEY = ''
-  YAHOO_API_KEY = ''
-  GOOGLE_API_KEY = 'ABQIAAAAFsjL_ySdbuJtGjezmrZK1xTJQa0g3IQ9GZqIMmInSLzwtGDKaBSiEsqQLyGI0BNbjNLcWmzxPaJyTQ'
-end
