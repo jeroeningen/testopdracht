377be7c5c3ff2f6baf43790c8b9befdff7dd9c7f
Jeroen van Ingen <jeroeningen@gmail.com>
2011-04-23 05:47:43 -0700

    Coffeescript added

---
+++ public/javascripts/application.coffee
@@ -0,0 +1,28 @@
+self.link_to_fancybox_or_not = (url, args) -> 
+	if $('input#enable_lightbox')[0].checked
+		$.fancybox({
+			href: url
+		})
+	else
+		document.location = url
+
+#this function needs to be globally available, 
+#because it is also used in the lightbox that is loaded later
+self.go_back =-> 
+	if $('input#enable_lightbox')[0].checked
+		$.fancybox.close()
+	else
+		history.back()
+		
+$(document).ready -> 
+	$('a.delete_link_fancybox').click ->
+		if $('input#enable_lightbox')[0].checked
+			$.fancybox({
+				href: this + '?enable_lightbox=1',
+				http_method: 'delete'
+			})
+			false
+		else
+			confirm('Are you sure?')
+	$('form#form_to_enable_lightbox').change ->
+		$('form#form_to_enable_lightbox').submit()
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,35 +1,34 @@
-function link_to_fancybox_or_not(url, args) {
-	if ($('input#enable_lightbox')[0].checked) {
-		$.fancybox({
-			href: url
-		})
-	} else {
-		document.location = url
-	}
-}
-
-//this function needs to be globally available, 
-//because it is also used in the lightbox that is loaded later
-function go_back() {
-	if ($('input#enable_lightbox')[0].checked) {
-		return $.fancybox.close()
-	} else {
-		return history.back()
-	}
-}
-$(document).ready(function() {
-	$('a.delete_link_fancybox').click(function() {
-		if ($('input#enable_lightbox')[0].checked) {
-			$.fancybox({
-				href: this + '?enable_lightbox=1',
-				http_method: 'delete'
-			})
-			return false
-		} else {
-			return confirm('Are you sure?')
-		}
-	})
-	$('form#form_to_enable_lightbox').change(function() {
-		$('form#form_to_enable_lightbox').submit()
-	})
-})
+(function() {
+  self.link_to_fancybox_or_not = function(url, args) {
+    if ($('input#enable_lightbox')[0].checked) {
+      return $.fancybox({
+        href: url
+      });
+    } else {
+      return document.location = url;
+    }
+  };
+  self.go_back = function() {
+    if ($('input#enable_lightbox')[0].checked) {
+      return $.fancybox.close();
+    } else {
+      return history.back();
+    }
+  };
+  $(document).ready(function() {
+    $('a.delete_link_fancybox').click(function() {
+      if ($('input#enable_lightbox')[0].checked) {
+        $.fancybox({
+          href: this + '?enable_lightbox=1',
+          http_method: 'delete'
+        });
+        return false;
+      } else {
+        return confirm('Are you sure?');
+      }
+    });
+    return $('form#form_to_enable_lightbox').change(function() {
+      return $('form#form_to_enable_lightbox').submit();
+    });
+  });
+}).call(this);
