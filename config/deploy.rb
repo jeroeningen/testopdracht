0c5e27e2aff2a5bfaa895f2a303158e93781e962
Jeroen van Ingen <jeroeningen@gmail.com>
2011-05-03 10:41:42 -0700

    Bundle install added for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,17 @@ role :web, "localhost"                          # Your HTTP server, Apache/etc
 role :app, "localhost"                          # This may be the same as your `Web` server
 role :db, "localhost", :primary => true
 
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-   task :symlink_shared do
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-   end
- end
-after 'deploy:update_code', 'deploy:symlink_shared', "deploy:migrate"
+  end
+  task :bundle_install do
+    system "bundle install"
+  end
+end
+after 'deploy:update_code', 'deploy:symlink_shared', "deploy:bundle_install", "deploy:migrate"
