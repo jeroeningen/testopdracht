30d55f02e59b86ecbb252ac118c918b24d6fa915
Jeroen van Ingen <jeroeningen@gmail.com>
2011-05-04 13:31:05 -0700

    Gemfile fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
+
 set :application, "testopdracht"
 set :repository,  "https://jeroeningen@github.com/jeroeningen/testopdracht.git"
 
 set :scm, :git
-set :deploy_to, "/rails"
+set :deploy_to, "/rails/testopdracht"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "localhost"                          # Your HTTP server, Apache/etc
-role :app, "localhost"                          # This may be the same as your `Web` server
-role :db, "localhost", :primary => true
+role :web, "localhost", "192.168.168.129"
+role :app, "localhost", "192.168.168.129"
+role :db, "localhost", "192.168.168.129", :primary => true
 
 namespace :deploy do
   task :start do ; end
@@ -19,7 +20,8 @@ namespace :deploy do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   task :bundle_install do
-    system "bundle install"
+    run "export PATH=$PATH:/var/lib/gems/1.8/bin/ && cd #{release_path} && bundle install --deployment --without test"
   end
 end
-after 'deploy:update_code', 'deploy:symlink_shared', "deploy:bundle_install", "deploy:migrate"
+
+after 'deploy:update_code', "deploy:bundle_install", 'deploy:symlink_shared', "deploy:migrate"
