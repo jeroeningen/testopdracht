b8e7de42ac7fbe1a4f88196f5bb89e5452c0d8c0
Jeroen van Ingen <jeroeningen@gmail.com>
2011-05-05 13:25:58 -0700

    Symlink update for apache

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -7,6 +7,7 @@ namespace :deploy do
     run "export PATH=$PATH:/var/lib/gems/1.8/bin/ && cd #{release_path} && bundle exec rake RAILS_ENV=development db:migrate"
   end
   task :symlink_passenger do
-    run "#{try_sudo} ln -s #{release_path}/public /var/www/#{application}_development" unless File.exists?("/var/www/#{application}_development")
+    run "#{try_sudo} rm -rf /var/www/#{application}_development" if File.exists?("/var/www/#{application}_development")
+    run "#{try_sudo} ln -s #{release_path}/public /var/www/#{application}_development"
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@ namespace :deploy do
     run "export PATH=$PATH:/var/lib/gems/1.8/bin/ && cd #{release_path} && bundle exec rake RAILS_ENV=production db:migrate"
   end
   task :symlink_passenger do
-    run "#{try_sudo} ln -s #{release_path}/public /var/www/#{application}_production" unless File.exists?("/var/www/#{application}_production")
+    run "#{try_sudo} rm -rf /var/www/#{application}_production" if File.exists?("/var/www/#{application}_production")
+    run "#{try_sudo} ln -s #{release_path}/public /var/www/#{application}_production"
   end
 end
